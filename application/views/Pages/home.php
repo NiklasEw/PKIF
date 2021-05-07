6b4b60c0a0155a7aba3d927b75d01fe1f707ea82
Black86Shadow <spiele86@freenet.de>
2021-05-07 17:35:39 +0200

    Home 1

--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -3,15 +3,24 @@
 
 <head>
 <script>
-    $(document).ready(function(e){
+    $(document).ready(function(e){   
         
         $session = $this->session->userdata('id_user');
-                if($("#updateid").val()!=""){
-                    var func="<?php echo site_url("db/frage_update");?>";
-                }
-                else{
-                    var func="<?php echo site_url("db/frage_create");?>";
-                }
+        if(!empty($session)){
+            $is_admin = "<div data-id=" . $data_item['QID'] . " ></div>";
+            <div data-id=" . $data_item['QID'] . "></div>;
+        }
+        else{
+        $is_admin = "";
+        }
+
+        if($("#updateid").val()!=""){
+            var func="<?php echo site_url("db/frage_update");?>";
+        }
+        else{
+            var func="<?php echo site_url("db/frage_create");?>";
+        }
+
                 
         //Macht den Frage stellen button aus der Eingabe der Datenbank funktionsfähig 
         //und den Controller Database zur Funktion create
@@ -23,11 +32,13 @@
                 data:$("#myForm").serialize(),
                 success: function (response) {
                     $("#myForm").trigger("reset");
+                    window.location.reload(); 
                     alert(response);
                 }
             });
         });
     });
+    
 
 </script>
 </head>
@@ -45,25 +56,28 @@
             <label for="exampleFormControlTextarea1">Beschreibung</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="Content"></textarea>
         </div>
-        <input type="hidden"id="updateid"name="id" value=""class="form-control">
+        <input type="hidden"id="updateid"name="QID" value=""class="form-control">
         <button id="FrageStellen" type="button" class="btn btn-primary pull-right">Frage stellen</button>
     </div>
 </form>
 
+</br>
+
    <!-- Ausgabe der Datenbank in Karten-->
    <div class="container" >
         <?php
-            foreach($QID AS $data_item) {
+       
+            foreach($Fragen AS $data_item) {
             echo'
-                    <div id="entry'.$data_item['id'].'" class="card">
-                        <div class="card-header" data-headline=" '. $data_item['Name']. '">
-                        '.$data_item['Name']. '
-                            <div data-id=" '.$data_item['id'].  ' " class="trash float-right"> <i class="fas fa-trash"></i> </div>
-                            <div class="edit float-right"> <i class="fas fa-edit"></i> </div>
+                    <div id="entry'.$data_item['QID'].'" class="card">
+                        <div class="card-header" data-headline=" '. $data_item['Headline']. '">
+                        '.$data_item['Headline']. '
+                            <div data-id=" '.$data_item['QID'].  ' "> </div>
+                            
                         <div class="card-body"  >  
                             <p class="card-text"
-                            data-content=" ' . $data_item['Eintrag'] .'">
-                            ' . $data_item['Eintrag'] .$data_item['id'].'
+                            data-content=" ' . $data_item['Content'] .'">
+                            ' . $data_item['Content'] .$data_item['QID'].'
                             </p> 
                         </div>
                     </div>
