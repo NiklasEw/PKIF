e97a77233751823510801438d11f0896c1372841
Black86Shadow <spiele86@freenet.de>
2021-05-18 15:08:34 +0200

    Bewertung

--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -6,15 +6,6 @@
     $(document).ready(function(e){   
 
         
-<<<<<<< HEAD
-        if($("#updateid").val()!=""){
-                    var func="<?php echo site_url("db/update_frage");?>";
-                }
-                else{
-                    var func="<?php echo site_url("db/create_frage");?>";
-                }
-                      
-=======
         $session = $this->session->userdata('id_user');
         if(!empty($session)){
             $is_admin = "<div data-id=" . $data_item['QID'] . " ></div>";
@@ -32,7 +23,6 @@
         }
 
                 
->>>>>>> 9e15c3faec44008273c58b0680dcc72f34125010
         //Macht den Frage stellen button aus der Eingabe der Datenbank funktionsfähig 
         $("#submit").click(function(){
             alert("Debug");
@@ -49,6 +39,38 @@
                 }
             });
         });
+
+         $("#good").click(function(){
+            alert("Debug");
+
+            $.ajax({
+                type:"POST",
+               
+                url: "<?php echo site_url('db/create_frage');?>",
+                data:$("#myForm").serialize(),
+                success: function (response) {
+                    $("#myForm").trigger("reset");
+                    window.location.reload(); 
+                    alert(response);
+                }
+            });
+        });
+
+        $("#bad").click(function(){
+            alert("Debug");
+
+            $.ajax({
+                type:"POST",
+               
+                url: "<?php echo site_url('db/');?>",
+                success: function (response) {
+                    $("#myForm").trigger("reset");
+                    window.location.reload(); 
+                    alert(response);
+                }
+            });
+        });
+
     });
     
 
@@ -86,7 +108,7 @@
             <label for="exampleFormControlTextarea1">Beschreibung</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="Content"></textarea>
         </div>
-        <input type="hidden"id="updateid"name="QID" value=""class="form-control">
+        <input type="hidden"id="updateid" name="QID" value=""class="form-control">
         <button id="submit" type="button" class="btn btn-primary pull-right">Frage stellen</button>
     </div>
 </form>
@@ -100,26 +122,35 @@
             echo'
                     <div id="entry'.$data_item['QID'].'" class="card">
                         <div class="card-header" data-headline=" '. $data_item['Headline']. '">
-                        '.$data_item['Headline']. $data_item['Time'] . $data_item['Username'] . '
+                        '.$data_item['QID'].' '. $data_item['Time'] .' '. $data_item['Username'] . ' ' .$data_item['Headline'].' 
                             <div data-id=" '.$data_item['QID'].  ' "> </div>
                             
                         <div class="card-body"  >  
                             <p class="card-text"
                             data-content=" ' . $data_item['Content'] .'">
-                            ' .$data_item['QID'].' '. $data_item['Content'].'
+                            '. $data_item['Content'].'
                             </p> 
                         </div>
                         <div class="card-body"  >  
                             <p class="card-text"
                             data-content=" ' . $data_item['negBewertung'] .'">
-                            <div align="right"> ' . $data_item['negBewertung'] .'</div> <div align="right"> ' . $data_item['posBewertung'] .'</div> 
+                            <div style="display:block; text-align:left; float:left;">' . $data_item['negBewertung']  .'
+                                <input type="hidden"id="updateid" name="QID" value=""class="form-control">
+                                <button id="good" type="button" class="btn btn-secondary pull-right">Hilfreich</button>
+                            </div>
+                            <div style="display:block; text-align:right;">' . $data_item['posBewertung'] .'
+                                <input type="hidden"id="updateid" name="QID" value=""class="form-control">
+                                <button id="bad" type="button" class="btn btn-secondary pull-right">Nich Hilfreich</button>
+                            </div> 
                             </p> 
                         </div>
                     </div>
                 </div>
-            '; 
-            }
-        ?>
+            ';
+        }
+         ?>
+            
+        
     </div>
 
 </body>
