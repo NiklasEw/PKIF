2e2a6c0e5206e8611392d75a3dc0382fb927d7d2
camkaymine <cameronkaymail@gmail.com>
2021-06-01 14:54:41 +0200

    impressum

---
+++ application/views/Pages/impressum.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+    <p><b>Impressum</b></p>
+    
+
+
+bestefrage.net GmbH<br>
+info[at]bestefrage.net<br>
+Erika-Mann-Straße 23<br>
+München, 80636<br>
+<br>
+T + 420 69 · 13 37 666 · 911<br>
+<br>
+Geschäftsführung: <br>
+Wibor Tiegraebe, Eiklas Kwes<br>
+<br>
+Sitz: Gibts doch garnicht<br>
+Handelsregister: <br>
+AG "city not found" HRB 165109<br>
+USt-ID-Nr.: DE 250068022<br>
+<br>
+Verantwortlich im Sinne von Paragraph 6 MDStV:<br>
+Wibor Tiegraebe, Eiklas Kwes<br>
+<br>
+Mitarbeiter von Strafverfolgungsbehörden können ihre Anfragen für Auskunftsersuchen im Sinne von § 5 Abs. 2 NetzDG über die ihnen mitgeteilten Kontaktdaten einreichen.<br>                        
+<br>
+Weitere Plattformen der bestefrage.net GmbH:<br>
+Surveylense.de<br>
+<br>
+<br>
+gutefrage ist nicht bereit und nicht verpflichtet an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.<br>
+<br>
+Bilder/Fotos/Grafiken: istockphoto.de, unsplash.com, pexels.com, Marek & Beier Fotografen marekbeier.de, Freepik von www.flaticon.com<br>
+<br>
+Copyright ©1988<br>
+Alle Rechte vorbehalten<br>
+<br>
+</body>
+</html>
